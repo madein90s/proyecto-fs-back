f58117884755c7e150948a1ee76a1ebc4d0d2e73
Tatiana <tiana@192.168.1.2>
2023-05-11 10:45:37 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+function requestController() {
+  //Lógica de nuestra función
+  console.log("Hello, World!");
+}
+
+// Configurar nuestro servidor
+const server = http.createServer(requestController);
+
+server.listen(4000);
